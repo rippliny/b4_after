5de6b5dbbdedc85538a7f95ccbac7be0ed958b31
정형빈 <gudqls369@gmail.com>
2022-10-17 20:55:06 +0900

    user, photo 앱 모델 완성

--- user/models.py
+++ user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class UserModel(AbstractUser):
+    class Meta:
+        db_table = "my_user"
+
+    first_name = models.CharField(max_length=16, null=False)
+    last_name = models.CharField(max_length=16, null=False)
+    
+    def __str__(self):
+        return self.username
