7f1f213dda031530c630df9c2dc5694e33df75ad
gracegoh <gracegoh924@gmail.com>
2022-10-17 20:23:56 +0900

    user 앱 model 생성

--- user/models.py
+++ user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class UserModel(AbstractUser):
+    class Meta:
+        db_table = "my_user"
+
+    first_name = models.CharFiled(null=False)
+    last_name = models.CharFiled(null=False)
