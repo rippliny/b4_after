c5ebab33a7f7f5063ad2bfdd0c5f21c0c3a5247e
정형빈 <gudqls369@gmail.com>
2022-10-19 21:09:29 +0900

    커밋 테스트

--- photo/models.py
+++ photo/models.py
@@ -9,9 +9,10 @@ class PhotoModel(models.Model):
     img = models.ImageField(upload_to='photo/', null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    category = models.CharField(max_length=16, null=True, default='')
     favorites = models.ManyToManyField(UserModel, related_name='favorites' ,blank=True)
     trash = models.ManyToManyField(UserModel, related_name='trash' ,blank=True)
-    category = models.CharField(max_length=16, null=True, default='')
+
 
     def __str__(self):
         return 'id : {}'.format(self.id)
